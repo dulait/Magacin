353ead3436f60d8e2044f70ef139c8816412e644
dulait <draskovicdusan4@gmail.com>
2024-03-04 15:34:20 +0100

    Dodata logicka kontrola za set metode u klasi Artikal

--- src/magacin/Artikal.java
+++ src/magacin/Artikal.java
@@ -11,24 +11,36 @@ public class Artikal {
 		return naziv;
 	}
 	public void setNaziv(String naziv) {
+		if(naziv == null) {
+			throw new NullPointerException("Naziv ne moze biti null");
+		}
 		this.naziv = naziv;
 	}
 	public int getSifra() {
 		return sifra;
 	}
 	public void setSifra(int sifra) {
+		if(sifra <= 0) {
+			throw new IllegalArgumentException("Vrednost sifre nije validna");
+		}
 		this.sifra = sifra;
 	}
 	public String getOpis() {
 		return opis;
 	}
 	public void setOpis(String opis) {
+		if(opis == null) {
+			throw new NullPointerException("Opis ne moze biti null");
+		}
 		this.opis = opis;
 	}
 	public int getKolicina() {
 		return kolicina;
 	}
 	public void setKolicina(int kolicina) {
+		if(kolicina < 0) {
+			throw new IllegalArgumentException("Kolicina ne sme biti manja od 0");
+		}
 		this.kolicina = kolicina;
 	}
 	@Override
